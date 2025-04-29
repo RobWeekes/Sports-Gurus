1b3f7bf67a22ff1e741531f16307436f843caad3
Robert Weekes <weekes.robert@gmail.com>
2025-04-28 21:11:58 -0700

    Added an API router

---
+++ backend/routes/api/index.js
@@ -0,0 +1,14 @@
+// backend/routes/api/index.js
+
+const router = require('express').Router();
+
+// All the URLs of the routes in the api router will be prefixed with /api
+
+// test route
+router.post('/test', function(req, res) {
+  res.json({ requestBody: req.body });
+});
+
+
+
+module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,6 +6,14 @@ const express = require('express');
 const router = express.Router();
 
 
+// All the API routes will be served at URL's starting with /api/
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
+// All the URLs of the routes in the api router will be prefixed with /api
+
+
 // In this test route, set a cookie on the response with the name 'XSRF-TOKEN' to the value of the req.csrfToken method's return. Then, send "Hello World!" as the response body
 // router.get('/hello/world', function (req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
