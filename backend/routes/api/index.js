ea0aa5756b95ee299f5c53560e7b87da7787622e
Robert Weekes <weekes.robert@gmail.com>
2025-04-28 21:13:08 -0700

    comment

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,10 +5,23 @@ const router = require('express').Router();
 // All the URLs of the routes in the api router will be prefixed with /api
 
 // test route
-router.post('/test', function(req, res) {
+router.post('/test', function (req, res) {
   res.json({ requestBody: req.body });
 });
 
 
 
 module.exports = router;
+
+
+
+// Example fetch request:
+
+// fetch('/api/test', {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
+//   },
+//   body: JSON.stringify({ hello: 'world' })
+// }).then(res => res.json()).then(data => console.log(data));
