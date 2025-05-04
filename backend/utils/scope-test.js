b9b1639d16b94f091e5de64b028151b6004fad33
Robert Weekes <weekes.robert@gmail.com>
2025-05-04 03:24:23 -0700

    set up package.json at root - so Render "npm install" will install packages for the backend

--- backend/utils/scope-test.js
+++ backend/utils/scope-test.js
@@ -1,42 +1,42 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const config = require('../config');
-const UserModel = require('../db/models/user');
+// const { Sequelize, DataTypes } = require('sequelize');
+// const config = require('../config');
+// const UserModel = require('../db/models/user');
 
-// Create a new Sequelize instance
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: config.dbFile || 'db/dev.db'
-});
+// // Create a new Sequelize instance
+// const sequelize = new Sequelize({
+//   dialect: 'sqlite',
+//   storage: config.dbFile || 'db/dev.db'
+// });
 
-// Initialize the User model directly
-const TestUser = UserModel(sequelize, DataTypes);
-
-
-
-// This file is for testing scope limiting fields in sequelize / possible backend route requests.
-
-// For testing "all fields" query, enable "withAllFields" scope in user model file. Otherwise: SequelizeScopeError: Invalid scope...
-
-// run this command in /backend:
-// node utils/scope-test.js
+// // Initialize the User model directly
+// const TestUser = UserModel(sequelize, DataTypes);
 
 
 
-async function testScope() {
-  try {
-    // This should exclude hashedPassword, email, createdAt, updatedAt
-    const users = await TestUser.findAll();
-    console.log("Default scope:", JSON.stringify(users, null, 2));
+// // This file is for testing scope limiting fields in sequelize / possible backend route requests.
 
-    // This will include all fields
-    // const usersWithAllFields = await TestUser.scope('withAllFields').findAll();
-    // console.log("All fields:", JSON.stringify(usersWithAllFields, null, 2));
-  } catch (error) {
-    console.error("Error:", error);
-  } finally {
-    // Close the connection
-    await sequelize.close();
-  }
-}
+// // For testing "all fields" query, enable "withAllFields" scope in user model file. Otherwise: SequelizeScopeError: Invalid scope...
 
-testScope();
+// // run this command in /backend:
+// // node utils/scope-test.js
+
+
+
+// async function testScope() {
+//   try {
+//     // This should exclude hashedPassword, email, createdAt, updatedAt
+//     const users = await TestUser.findAll();
+//     console.log("Default scope:", JSON.stringify(users, null, 2));
+
+//     // This will include all fields
+//     // const usersWithAllFields = await TestUser.scope('withAllFields').findAll();
+//     // console.log("All fields:", JSON.stringify(usersWithAllFields, null, 2));
+//   } catch (error) {
+//     console.error("Error:", error);
+//   } finally {
+//     // Close the connection
+//     await sequelize.close();
+//   }
+// }
+
+// testScope();
