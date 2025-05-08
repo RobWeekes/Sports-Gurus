ae5b1da8adc5482c84a0bb20e60a9622053e0f58
Robert Weekes <weekes.robert@gmail.com>
2025-05-07 19:20:12 -0700

    set up frontend/src/main.jsx to access the store, wrapping <App /> with <Provider > </Provider>

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,29 @@
+// frontend/src/main.jsx
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+// using Provider from Redux to provide the Redux store
+import { Provider } from 'react-redux';
+import configureStore from './store';
+
+
+// Create a variable to access the store and expose it on the window.
+// It should not be exposed in production
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
+// Wrap the rendered App component in Redux's Provider component, passing store as a prop of the same name to the Provider
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -3,9 +3,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
+
 const rootReducer = combineReducers({
 
 });
+
 
 // Initialize an enhancer variable that will be set to different store enhancers depending on whether the Node environment is development or production.
 
@@ -22,6 +24,7 @@ if (import.meta.env.MODE === "production") {
 }
 
 // In development, also apply the logger middleware and the Redux DevTools" compose enhancer. To use these tools, create a logger variable that uses the default export of redux-logger. (Use await, a dynamic import, for this.) Then, grab the Redux DevTools compose enhancer with window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ and store it in a variable called composeEnhancers. Use an or (||) to keep the Redux"s original compose as a fallback in case the Redux DevTools are not installed. Then set the enhancer variable to the return of the composeEnhancers function passing in applyMiddleware invoked with thunk then logger.
+
 
 const configureStore = (preloadedState) => {
   return createStore(rootReducer, preloadedState, enhancer);
