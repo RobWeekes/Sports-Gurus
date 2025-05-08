852968b00a5a2d046632a9b8f6b77cd1c12409ef
Robert Weekes <weekes.robert@gmail.com>
2025-05-08 02:10:57 -0700

    set up /api proxy and csrfFetch function, to add the XSRF-TOKEN cookie to headers on non "GET" requests

---
+++ frontend/src/store/csrf.js
@@ -0,0 +1,27 @@
+// frontend/src/store/csrf.js
+
+import Cookies from "js-cookie";
+
+// To make fetch requests with any HTTP verb other than GET, set an XSRF-TOKEN header on the request. The value of the header should be set to the XSRF-TOKEN cookie. To do this, wrap the fetch function on the window that will be used in place of the default fetch function.
+
+export async function csrfFetch(url, options = {}) {
+  // if options.method is not set, set it to the GET method
+  options.method = options.method || "GET";
+  // if options.headers is not set, default it to an empty object
+  options.headers = options.headers || {};
+
+  // if the options.method is not "GET", then set the "Content-Type" header to "application/json", and set the "XSRF-TOKEN" header to the value of the "XSRF-TOKEN" cookie
+  if (options.method.toUpperCase() !== "GET") {
+    options.headers["Content-Type"] =
+      options.headers["Content-Type"] || "application/json";
+    options.headers["XSRF-Token"] = Cookies.get("XSRF-TOKEN");
+  }
+  // call the default window"s fetch with the url and the options passed in
+  const res = await window.fetch(url, options);
+
+  // if the response status code is 400 or above, then throw an error with the error being the response
+  if (res.status >= 400) throw res;
+
+  // if the response status code is under 400, then return the response to the next promise chain
+  return res;
+}
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,9 +11,21 @@ export default defineConfig(({ mode }) => ({
       failOnError: mode === "production"
     })
   ],
-  // To automatically open the app in the browser whenever the server starts,
-  // uncomment the following lines:
-  // server: {
-  //   open: true
-  // }
+  server: {
+    proxy: {
+      "/api": "http://localhost:8000"
+    }
+  } // proxy object: routes: urls to be redirected
 }));
+
+// This proxy will force the frontend server to act like it's being served from the backend server. So if you do a fetch request in the React frontend like fetch('/api/csrf/restore), then the GET /api/csrf/restore request will be made to the backend server instead of the frontend server.
+
+// The Express backend server is configured to be CSRF protected and will only accept requests that have the right CSRF secret token in a header, and the right CSRF token value in a cookie.
+
+
+
+// To automatically open the app in the browser whenever the server starts,
+// add the following key:
+// server: {
+//   open: true
+// }
