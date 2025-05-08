f5697ca2e54c88df3e1f85326997bf3f621c5192
Robert Weekes <weekes.robert@gmail.com>
2025-05-08 07:52:11 -0700

    created LoginFormPage component and browser router with /login route

---
+++ frontend/src/components/LoginFormPage/LoginFormPage.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import * as sessionActions from '../../store/session';
+
+
+function LoginFormPage() {
+  const dispatch = useDispatch();
+  const sessionUser = useSelector((state) => state.session.user);
+  const [credential, setCredential] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({});
+
+  // if user is already logged in when trying to access LoginFormPage, redirect them to the / path
+  if (sessionUser) return <Navigate to="/" replace={true} />;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors({});
+    return dispatch(sessionActions.login({ credential, password })).catch(
+      async (res) => {
+        const data = await res.json();
+        if (data?.errors) setErrors(data.errors);
+      }
+    );
+  };  // On submit of the form, dispatch the login thunk action with the form input values. Display errors if needed
+
+  // Render a form with a controlled input for the user login credential (username or email) and the password
+  return (
+    <>
+      <h1>Log In</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username or Email
+          <input
+            type="text"
+            value={credential}
+            onChange={(e) => setCredential(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        {errors.credential && <p>{errors.credential}</p>}
+        <button type="submit">Log In</button>
+      </form>
+    </>
+  );
+}
+
+
+
+export default LoginFormPage;
