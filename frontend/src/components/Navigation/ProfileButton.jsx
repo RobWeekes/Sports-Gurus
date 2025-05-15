acc4d3596158085588238a29e5132c20fe3fd320
Robert Weekes <weekes.robert@gmail.com>
2025-05-15 06:07:24 -0700

    comments

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -12,40 +12,46 @@ function ProfileButton({ user }) {
   const dispatch = useDispatch();
   const sessionUser = useSelector(state => state.session.user);
   console.log("ProfileButton rendering with user:", sessionUser);
-
   const [showMenu, setShowMenu] = useState(false);
   const ulRef = useRef();
 
   const toggleMenu = (e) => {
+    // keep click from bubbling up to document and triggering closeMenu
     e.stopPropagation();
     setShowMenu(!showMenu);
   };
 
   useEffect(() => {
+    // only want the document-wide click listener if the dropdown is open
     if (!showMenu) return;
 
+    // change showMenu to false only if the target of the click event does NOT contain the HTML element of the dropdown menu
     const closeMenu = (e) => {
       if (ulRef.current && !ulRef.current.contains(e.target)) {
         setShowMenu(false);
       }
     };
 
+    // add the event listener & return cleanup only if showMenu is true
     document.addEventListener("click", closeMenu);
     return () => document.removeEventListener("click", closeMenu);
   }, [showMenu]);
-
-  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
 
   const logout = (e) => {
     e.preventDefault();
     dispatch(sessionActions.logout());
   };
 
+  // the ul elements (user info) will be "hidden" or not
+  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
+
   return (
     <>
       <button onClick={toggleMenu} className="profile-button">
         <SportIcon sporticon={user.sportIcon || "usercircle"} size="1.5em" />
       </button>
+
       <ul className={ulClassName} ref={ulRef}>
         <li>{user.userName}</li>
         <li>{user.firstName} {user.lastName}</li>
@@ -57,6 +63,7 @@ function ProfileButton({ user }) {
           <button onClick={logout}>Log Out</button>
         </li>
       </ul>
+
     </>
   );
 }
