061e737b9b523f0012bce4a52d371acf42d33554
Robert Weekes <weekes.robert@gmail.com>
2025-05-16 21:29:16 -0700

    added modal button components to ProfileButton, added onButtonClick={closeMenu} props to close drop down menu when Log In/Sign Up/Log Out buttons are clicked - done with phase5 except for optional OpenModalMenuItem

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,59 +1,66 @@
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
-import OpenModalButton from "../OpenModalButton";
-import LoginFormModal from "../LoginFormModal/LoginFormModal";
-import SignupFormModal from "../SignupFormModal/SignupFormModal";
+// import OpenModalButton from "../OpenModalButton";
+// import LoginFormModal from "../LoginFormModal/LoginFormModal";
+// import SignupFormModal from "../SignupFormModal/SignupFormModal";
 import "./Navigation.css";
 
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
+
   console.log("Navigation rendering with isLoaded:", isLoaded);
   console.log("sessionUser:", sessionUser);
   console.log("Redux state:", useSelector(state => state));
 
-  // if logged in ? render Profile button, Log Out button...
-  const sessionLinks = sessionUser ? (
-    <li>
-      {/* remove <div>Test</div> when fixed: */}
-      {/* <div>Test User Logged In</div> */}
-      <ProfileButton user={sessionUser} />
-    </li>
-    // otherwise render Log In, Sign Up links/modal components
-  ) : (
-    <>
-      <li>
-        {/* remove <div>Test</div> when fixed: */}
-        {/* <div>Test User Not Logged In</div> */}
-        <OpenModalButton
-          buttonText="Log In"
-          modalComponent={<LoginFormModal />}
-        />
-      </li>
-      <li>
-        {/* <NavLink to="/signup">Sign Up</NavLink> */}
-        <OpenModalButton
-          buttonText="Sign Up"
-          modalComponent={<SignupFormModal />}
-        />
-      </li>
-    </>
-  );
-
   return (
     <ul>
       <li>
-        {/* always render "Home" link */}
         <NavLink to="/">Home</NavLink>
       </li>
       {/* after user session has loaded, render additional elements */}
-      {isLoaded && sessionLinks}
-      {/* Troubleshooting: render sessionLinks regardless of isLoaded */}
-      {/* {sessionLinks} */}
+      {isLoaded && (
+        <li>
+          <ProfileButton user={sessionUser} />
+        </li>
+      )}
     </ul>
   );
 }
 
 
 export default Navigation;
+
+
+
+// phase5- nesting Log In / Sign Up modals into ProfileButton- as a modal
+// Refactor Navigation to render the ProfileButton component even if there is no session user
+
+// // if logged in ? render Profile button, Log Out button...
+// const sessionLinks = sessionUser ? (
+//   <li>
+//     {/* remove <div>Test</div> when fixed: */}
+//     {/* <div>Test User Logged In</div> */}
+//     <ProfileButton user={sessionUser} />
+//   </li>
+//   // otherwise render Log In, Sign Up links/modal components
+// ) : (
+//   <>
+//     <li>
+//       {/* remove <div>Test</div> when fixed: */}
+//       {/* <div>Test User Not Logged In</div> */}
+//       <OpenModalButton
+//         buttonText="Log In"
+//         modalComponent={<LoginFormModal />}
+//       />
+//     </li>
+//     <li>
+//       {/* <NavLink to="/signup">Sign Up</NavLink> */}
+//       <OpenModalButton
+//         buttonText="Sign Up"
+//         modalComponent={<SignupFormModal />}
+//       />
+//     </li>
+//   </>
+// );
--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -4,6 +4,9 @@ import { useState, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from 'react-router-dom';
 import * as sessionActions from "../../store/session";
+import OpenModalButton from "../OpenModalButton";
+import LoginFormModal from "../LoginFormModal/LoginFormModal";
+import SignupFormModal from "../SignupFormModal/SignupFormModal";
 import SportIcon from "../SportIcon/SportIcon";
 import "./ProfileButton.css"
 // testing OpenModalButton: Greeting
@@ -29,57 +32,79 @@ function ProfileButton({ user }) {
 
     // change showMenu to false only if the target of the click event does NOT contain the HTML element of the dropdown menu
     const closeMenu = (e) => {
-      if (ulRef.current && !ulRef.current.contains(e.target)) {
+      if (!ulRef.current.contains(e.target)) {
         setShowMenu(false);
       }
     };
 
     // add the event listener & return cleanup only if showMenu is true
     document.addEventListener("click", closeMenu);
+
     return () => document.removeEventListener("click", closeMenu);
   }, [showMenu]);
+
+  // *added another closeMenu function and passed it to the onButtonClick prop of the OpenModalButton components, to close the dropdown when the buttons are clicked
+  const closeMenu = () => setShowMenu(false);
 
   const logout = (e) => {
     e.preventDefault();
     dispatch(sessionActions.logout());
+    // *test functionality - dropdown should close when buttons are clicked
+    closeMenu();
   };
 
-  // the ul elements (user info) will be "hidden" or not
+  // the user info will be "hidden" or not
   const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
 
 
   return (
     <>
       <button onClick={toggleMenu} className="profile-button" aria-label="User menu">
-        <SportIcon sporticon={user.sportIcon || "usercircle"} size="1.5em" />
+        <SportIcon sporticon={user ? user.sportIcon || "usercircle" : "usercircle"} size="1.5em" />
       </button>
-
-      {/* TESTING GREETING MODAL */}
-      {/* <Greeting /> */}
-
       <ul className={ulClassName} ref={ulRef}>
-        <div className="user-info">
-          <div className="user-name">{user.userName}</div>
-          <div className="user-full-name">{user.firstName} {user.lastName}</div>
-          <div className="user-email">{user.email}</div>
-        </div>
+        {user ? (
+          <>
+            <div className="user-info">
+              <div className="user-name">{user.userName}</div>
+              <div className="user-full-name">{user.firstName} {user.lastName}</div>
+              <div className="user-email">{user.email}</div>
+            </div>
 
-        <div className="menu-items">
-          <li>
-            <NavLink to="/profile/settings">My Profile</NavLink>
-          </li>
-          <li>
-            <NavLink to="/my-teams">My Teams</NavLink>
-          </li>
-          <li>
-            <NavLink to="/my-predictions">My Predictions</NavLink>
-          </li>
-          <li>
-            <button onClick={logout} className="logout-button">Sign Out</button>
-          </li>
-        </div>
+            <div className="menu-items">
+              <li>
+                <NavLink to="/profile/settings">My Profile</NavLink>
+              </li>
+              <li>
+                <NavLink to="/my-teams">My Teams</NavLink>
+              </li>
+              <li>
+                <NavLink to="/my-predictions">My Predictions</NavLink>
+              </li>
+              <li>
+                <button onClick={logout} className="logout-button">Sign Out</button>
+              </li>
+            </div>
+          </>
+        ) : (
+          <>
+            <li>
+              <OpenModalButton
+                buttonText="Log In"
+                onButtonClick={closeMenu}
+                modalComponent={<LoginFormModal />}
+              />
+            </li>
+            <li>
+              <OpenModalButton
+                buttonText="Sign Up"
+                onButtonClick={closeMenu}
+                modalComponent={<SignupFormModal />}
+              />
+            </li>
+          </>
+        )}
       </ul>
-
     </>
   );
 }
@@ -87,3 +112,80 @@ function ProfileButton({ user }) {
 
 
 export default ProfileButton;
+
+
+
+
+// // ** COMMENT THIS OUT \/ **
+// // if logged in ? render Profile button, Log Out button...
+// const sessionLinks = sessionUser ? (
+//   <li>
+//     {/* remove <div>Test</div> when fixed: */}
+//     {/* <div>Test User Logged In</div> */}
+//     <ProfileButton user={sessionUser} />
+//   </li>
+//   // otherwise render Log In, Sign Up links/modal components
+// ) : (
+//   <>
+//     <li>
+//       {/* remove <div>Test</div> when fixed: */}
+//       {/* <div>Test User Not Logged In</div> */}
+//       <OpenModalButton
+//         buttonText="Log In"
+//         modalComponent={<LoginFormModal />}
+//       />
+//     </li>
+//     <li>
+//       {/* <NavLink to="/signup">Sign Up</NavLink> */}
+//       <OpenModalButton
+//         buttonText="Sign Up"
+//         modalComponent={<SignupFormModal />}
+//       />
+//     </li>
+//   </>
+// );
+// // ** COMMENT THIS OUT ^^ **
+
+
+// return (
+//   <>
+//     <button onClick={toggleMenu} className="profile-button" aria-label="User menu">
+//       <SportIcon sporticon={user.sportIcon || "usercircle"} size="1.5em" />
+//     </button>
+
+//     {/* render session links - after loading? */}
+//     {sessionLinks}
+
+//     {/* TESTING GREETING MODAL */}
+//     {/* <Greeting /> */}
+
+//     {/* <ul className={ulClassName} ref={ulRef}>
+//       <div className="user-info">
+//         <div className="user-name">{user.userName}</div>
+//         <div className="user-full-name">{user.firstName} {user.lastName}</div>
+//         <div className="user-email">{user.email}</div>
+//       </div>
+
+//       <div className="menu-items">
+//         <li>
+//           <NavLink to="/profile/settings">My Profile</NavLink>
+//         </li>
+//         <li>
+//           <NavLink to="/my-teams">My Teams</NavLink>
+//         </li>
+//         <li>
+//           <NavLink to="/my-predictions">My Predictions</NavLink>
+//         </li>
+//         <li>
+//           <button onClick={logout} className="logout-button">Sign Out</button>
+//         </li>
+//       </div>
+//     </ul> */}
+
+//   </>
+// );
+
+
+
+
+// export default ProfileButton;
